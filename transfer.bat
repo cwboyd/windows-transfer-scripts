31651ad6bf67cf5004bd3c65167e4d4640c09a4a
cwboyd <cwboyd@users.noreply.github.com>
2022-05-07 09:04:48 -0500

    Default _excluded directory, to provide a place to move files to so they wont be transferred

--- transfer.bat
+++ transfer.bat
@@ -1,6 +1,7 @@
 
 SET __TARGET=p:\directory
-REM SET __EXCLUDES=/XD "dir1" "dir2"
+REM SET __EXCLUDES=/XD "_excluded" "dir1" "dir2"
+SET __EXCLUDES=/XD "_excluded"
 robocopy . %__TARGET% /MIR /XJD %__EXCLUDES%
 SET __TARGET=
 SET __EXCLUDES=
